aa1274e78e10dbdcd84b734cbc250146e6068607
Iryu-0817 <iryu20030817@gmail.com>
2025-04-05 22:41:38 +0900

    パスパラメータを取得

--- myapi/handlers/handlers.go
+++ myapi/handlers/handlers.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
@@ -27,13 +30,13 @@ func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	articleID := 1
-	resString := fmt.Sprintf("Article No.%d\n", articleID)
-	if req.Method == http.MethodGet {
-		io.WriteString(w, resString)
-	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
+	if err != nil {
+		http.Error(w, "Invalid query parameter", http.StatusMethodNotAllowed)
+		return
 	}
+	resString := fmt.Sprintf("Article No.%d\n", articleID)
+	io.WriteString(w, resString)
 }
 
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
