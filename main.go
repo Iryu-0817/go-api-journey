594ea7bf43f58c1e511f201ce90ef5b48190e1aa
Iryu-0817 <iryu20030817@gmail.com>
2025-04-05 18:36:14 +0900

    関数のリファクタリング

--- main.go
+++ main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/Iryu-0817/go-api-journey/myapi/handlers"
 )
 
 func main() {
 
-	http.HandleFunc("/hello", HelloHandler)
-	http.HandleFunc("/article", PostArticleHandler)
-	http.HandleFunc("/article/list", ArticleListHandler)
-	http.HandleFunc("/article/1", ArticleDetailHandler)
-	http.HandleFunc("/article/nice", PostNiceHandler)
-	http.HandleFunc("/comment", PostCommentHandler)
+	http.HandleFunc("/hello", handlers.HelloHandler)
+	http.HandleFunc("/article", handlers.PostArticleHandler)
+	http.HandleFunc("/article/list", handlers.ArticleListHandler)
+	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	http.HandleFunc("/comment", handlers.PostCommentHandler)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
