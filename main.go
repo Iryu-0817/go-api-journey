b1e068aa097391f5750f3d9dc58a223e3d90285d
Iryu-0817 <iryu20030817@gmail.com>
2025-04-13 18:53:01 +0900

    サーバーを立てられるようにもとの状態に戻す

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"net/url"
+	"log"
+	"net/http"
+
+	"github.com/Iryu-0817/go-api-journey/myapi/handlers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	u, _ := url.Parse("http://localhost:8080/?page=1&page=2&a=1&")
+	r := mux.NewRouter()
 
-	queryMap := u.Query()
-	fmt.Println(queryMap["page"])
-	fmt.Println(queryMap["a"])
-	fmt.Println(queryMap["b"])
+	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/1", handlers.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
+	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+
+	log.Println("server start at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+
 }
